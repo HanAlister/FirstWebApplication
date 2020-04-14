006b304050d4d361e4d3bf013f01f1a8306c267a
HanAlister <tokaruk.b@wp.pl>
2020-04-15 01:48:30 +0200

    OpenWeather api get forecast

--- FirstWebApplication/Controllers/WeatherForecastController.cs
+++ FirstWebApplication/Controllers/WeatherForecastController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.Xml;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace FirstWebApplication.Controllers
 {
@@ -11,6 +15,10 @@ namespace FirstWebApplication.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        private const string URL = "https://api.openweathermap.org/data/2.5/forecast";
+        private string urlParameters = "?appid=5e26fb10acb3dc8c3ace4a6f68913ee9&lang=pl";
+
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -24,16 +32,28 @@ namespace FirstWebApplication.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<IEnumerable<Object>> GetAsync()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            using (HttpClient client = new HttpClient())
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                client.BaseAddress = new Uri(URL);
+                HttpResponseMessage response = client.GetAsync(urlParameters + $"&id=3081368&units=metric").Result;
+
+                response.EnsureSuccessStatusCode();
+
+                var responseBody = await response.Content.ReadAsStringAsync();
+                var jsonObject = JsonConvert.DeserializeObject<WeatherForecast>(responseBody);
+
+                return jsonObject.List.Select(fd => new { 
+                    date = fd.DtTxt.ToString("dd/MM/yyyy hh:mm"),
+                    fd.Main.Temp, 
+                    fd.Main.Pressure, 
+                    fd.Main.Humidity, 
+                    Clouds = fd.Clouds?.All, 
+                    Rain = fd.Rain?.The3H.ToString() ?? "Brak", 
+                    Snow = fd.Snow?.The3H.ToString() ?? "Brak", 
+                    fd.Weather.FirstOrDefault().Description });
+            }
         }
     }
 }
